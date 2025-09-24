c8489b8002c56d1cd470d30d9a0acbceed41b394
venkatyashwanth <mudiliyashwanth@gmail.com>
2025-09-24 20:17:25 +0530

    fixed scss issues

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,11 +4,16 @@ import AppContainer from "./AppContainer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Add favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Optional: SVG or PNG variants */}
+        {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
+        {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png" /> */}
+      </head>
       <body>
         <AuthProvider>
-          <AppContainer>
-            {children}
-          </AppContainer>
+          <AppContainer>{children}</AppContainer>
         </AuthProvider>
       </body>
     </html>
